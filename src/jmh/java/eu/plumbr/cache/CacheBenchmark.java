b45c4fdfaf1215152326855acc2a1a9fd6427979
fbelov <fbelov@gramant.ru>
2019-04-26 15:13:04 +0300

    cache caffeine

--- src/jmh/java/eu/plumbr/cache/CacheBenchmark.java
+++ src/jmh/java/eu/plumbr/cache/CacheBenchmark.java
@@ -1,54 +1,66 @@
 package eu.plumbr.cache;
 
-import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+import org.apache.commons.lang.RandomStringUtils;
+import org.openjdk.jmh.annotations.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Thread)
+@Warmup(iterations = 0, time = 3)
+@Measurement(iterations = 2, time = 5)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class CacheBenchmark {
 
-  private CacheApplication.Service service;
+    private CacheApplication.Service service;
 
-  @Setup
-  public void prepare() {
-    ConfigurableApplicationContext context = SpringApplication.run(CacheApplication.class);
-    service = context.getBean("service", CacheApplication.Service.class);
+    @Setup
+    public void setup() {
+        ConfigurableApplicationContext context = SpringApplication.run(CacheApplication.class);
 
-  }
+        service = context.getBean("service", CacheApplication.Service.class);
+    }
 
-  @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public long nocache() {
-    return service.noCache("const");
-  }
+    @TearDown
+    public void tearDown() {
+        System.out.println("Annotation calls: " + service.getAnnotationCalls());
+        System.out.println("Caffeine calls: " + service.getCaffeineCalls());
+    }
 
-  @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public long annotationBased() {
-    return service.annotationBased("const", "another");
-  }
+    @Benchmark
+    public String nocache() {
+        return service.noCache(getRandom(), getRandom());
+    }
 
-  @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public long spel() {
-    return service.annotationWithSpel("const", "another");
-  }
+    @Benchmark
+    public String annotationBased() {
+        return service.annotationBased(getRandom(), getRandom());
+    }
 
-  @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public long manual() {
-    return service.manual("const");
-  }
+    @Benchmark
+    public String keyBased() {
+        return service.keyBased(new CacheApplication.Service.Key(getRandom(), getRandom()));
+    }
+
+    @Benchmark
+    public String caffeineBased() {
+        return service.caffeineBased(getRandom(), getRandom());
+    }
+
+    @Benchmark
+    public String spel() {
+        return service.annotationWithSpel(getRandom(), getRandom());
+    }
+
+    @Benchmark
+    public long manual() {
+        return service.manual(getRandom());
+    }
+
+    private String getRandom() {
+        return RandomStringUtils.randomAlphabetic(2).toLowerCase();
+    }
 
 }
--- src/main/java/eu/plumbr/cache/CacheApplication.java
+++ src/main/java/eu/plumbr/cache/CacheApplication.java
@@ -1,5 +1,10 @@
 package eu.plumbr.cache;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.Cache;
@@ -7,6 +12,9 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 @EnableCaching
@@ -17,26 +25,50 @@ public class CacheApplication {
   }
 
   @Component("service")
+  @Getter
   public static class Service {
 
     private final Cache cache;
+    private final LoadingCache<Key, String> loadingCache;
+
+    private AtomicLong caffeineCalls = new AtomicLong();
+    private AtomicLong annotationCalls= new AtomicLong();
 
     public Service(CacheManager cacheManager) {
       cache = cacheManager.getCache("time");
+
+      loadingCache = Caffeine.newBuilder()
+              .expireAfterWrite(100, TimeUnit.MILLISECONDS)
+              .build(key -> {
+                caffeineCalls.incrementAndGet();
+
+                return doGetTime(key.a, key.b);
+              });
     }
 
-    public long noCache(String dummy) {
-      return System.currentTimeMillis();
+    public String noCache(String a, String b) {
+      return doGetTime(a, b);
     }
 
     @Cacheable("time")
-    public long annotationBased(String dummy1, String dummy2) {
-      return System.currentTimeMillis();
+    public String annotationBased(String a, String b) {
+      return doGetTime(a, b);
+    }
+
+    @Cacheable("time")
+    public String keyBased(Key key) {
+      annotationCalls.incrementAndGet();
+
+      return doGetTime(key.a, key.b);
+    }
+
+    public String caffeineBased(String a, String b) {
+      return loadingCache.get(new Key(a, b));
     }
 
     @Cacheable(value = "time", key = "#p0.concat(#p1)")
-    public long annotationWithSpel(String dummy1, String dummy2) {
-      return System.currentTimeMillis();
+    public String annotationWithSpel(String dummy1, String dummy2) {
+      return doGetTime(dummy1, dummy1);
     }
 
     public long manual(String dummy) {
@@ -49,6 +81,17 @@ public class CacheApplication {
         result = (long) valueWrapper.get();
       }
       return result;
+    }
+
+    private String doGetTime(String a, String b) {
+      return a + b;
+    }
+
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    public static class Key {
+      private String a;
+      private String b;
     }
   }
 }
